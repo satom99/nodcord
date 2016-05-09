a159a1382e0d7be9beae08faaefe2782f1950c27
AdamJames <santitm99@gmail.com>
2016-05-09 18:06:18 +0200

    Storage
    - Initial structures commit
    - Initial storage commit
    - Added storage routines to Client
    - Added core events, mainly meant for storage, to Client
    - Added send method to Socket
    - Modified merge utility
    - Moved large_threshold to defaultSettings

--- examples/test.js
+++ examples/test.js
@@ -3,6 +3,17 @@ var bot = new nodcord();
 
 bot.on('message_create', (message) => console.log(message.author.username+': '+message.content));
 
+bot.on('ready', () => {
+	bot.setStats(
+		{
+			game: '',
+			/*username: '',
+			avatar: '',*/
+		}
+	);
+	console.log(bot.user.id);
+});
+
 bot.login({
 	token: '',
 	/*email: '',
